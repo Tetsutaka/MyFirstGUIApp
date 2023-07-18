0d1ce72bb52ec83260d2c0ee25d678ec93688ff8
Tetsutaka <couches.omicron0f@icloud.com>
2023-07-19 00:15:25 +0900

    fixed bug and added some comments

--- rename.py
+++ rename.py
@@ -8,34 +8,32 @@ DEBUG_DIR = "debug"
 TEST = False
 
 class RenameApp:
-    def __init__(self, path_to_sample_data) -> None:
+    def __init__(self, path_to_sample_data: str) -> None:
         self.path_to_sample_data = path_to_sample_data
         self.set_logging()
 
     def set_logging(self):
+        # Set logger
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
         stream_handler = logging.StreamHandler()
         self.logger.addHandler(stream_handler)
         file_handler = logging.FileHandler("sample.log")
         self.logger.addHandler(file_handler)
-    
-    def error_handler():
-        pass
 
-    def copy_data_for_debug(self, new_path):
+    def copy_data_for_debug(self, new_path: str):
         files = glob.glob(self.path_to_sample_data)
-        if len(files) == 0:
-            self.logger.error(f"no files found in '{self.path_to_sample_data}'")
+        if len(files) == 0:  # Exit process if no file or directory found.
+            self.logger.error(f"no file or directory found in '{self.path_to_sample_data}'")
             return
-        if not os.path.exists(new_path):
+        if not os.path.exists(new_path):  # Create directory if there is no destination directory.
             self.logger.info(f"Created directory '{new_path}'")
             os.makedirs(new_path, exist_ok=True)
         for file in files:
-            if TEST:
+            if TEST:  # If it is test mode, it exits without processing any file or directory.
                 self.logger.info(f"DEBUG: Copied directory '{file}' to '{new_path}'")
                 continue
-            shutil.copy(file, new_path)
+            shutil.copytree(file, os.path.join(new_path, file), dirs_exist_ok=True)
             self.logger.info(f"Copied directory '{file}' to '{new_path}'")
 
 
